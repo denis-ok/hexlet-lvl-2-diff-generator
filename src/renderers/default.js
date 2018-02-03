336dd95e3e6a8f169b2ef5452bfaf0c10890e76d
denis-ok <strelkov.d.d@mail.ru>
2018-02-03 14:26:39 +0300

    refactor default diff renderer

--- src/renderers/default.js
+++ src/renderers/default.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline, no-use-before-define */
 import _ from 'lodash';
 
 const indent = '  ';
@@ -6,14 +7,14 @@ const markAdd = '+ ';
 const newline = '\n';
 const getIndent = num => _.repeat(indent, num);
 
-const printObj = (obj, level) => {
-  const keys = Object.keys(obj);
 
-  const result = keys.map((k) => {
+const objToString = (obj, level) => {
+  const allKeys = Object.keys(obj);
+  const result = allKeys.map((k) => {
     const value = obj[k];
 
     if (_.isObject(value)) {
-      return `${newline}${getIndent(level + 1)}${indent}${k}: ${printObj(value, level + 1)}`;
+      return `${newline}${getIndent(level + 1)}${indent}${k}: ${objToString(value, level + 1)}`;
     }
 
     return `${newline}${getIndent(level + 1)}${indent}${k}: ${value}`;
@@ -22,43 +23,42 @@ const printObj = (obj, level) => {
   return `{${result.join('')}${newline}${getIndent(level)}}`;
 };
 
-const defaultAction = (obj, level, mark) => {
-  const { name, valueNew, valueOld } = obj;
+
+const makeAction = (obj, level) => {
+  const { name, type, valueNew, valueOld, valueDeep } = obj;
   const value = !valueOld ? valueNew : valueOld;
-  const action = _.isObject(value) ? printObj(value, level + 1) : value;
-  return `${getIndent(level)}${mark}${name}: ${action}${newline}`;
+
+  const actionSimple = (mark) => {
+    const action = _.isObject(value) ? objToString(value, level + 1) : value;
+    return `${getIndent(level)}${mark}${name}: ${action}${newline}`;
+  };
+
+  const typeToString = {
+    unmodified: () => actionSimple(indent),
+    added: () => actionSimple(markAdd),
+    removed: () => actionSimple(markRemove),
+
+    modified: () => {
+      const stringNew = `${getIndent(level)}${markAdd}${name}: ${valueNew}${newline}`;
+      const stringOld = `${getIndent(level)}${markRemove}${name}: ${valueOld}${newline}`;
+      return `${stringNew}${stringOld}`;
+    },
+
+    object: () => {
+      const stringKey = `${getIndent(level)}${indent}${name}: `;
+      const stringValue = `${buildDiffString(valueDeep, level + 1)}${newline}`;
+      return `${stringKey}${stringValue}`;
+    },
+  };
+
+  const action = typeToString[type];
+  return action();
 };
 
-const actionUnmodified = (obj, level) => defaultAction(obj, level, indent);
-const actionAdded = (obj, level) => defaultAction(obj, level, markAdd);
-const actionRemoved = (obj, level) => defaultAction(obj, level, markRemove);
-
-const actionModified = (obj, level) => {
-  const { name, valueNew, valueOld } = obj;
-  return `${getIndent(level)}${markAdd}${name}: ${valueNew}${newline}${getIndent(level)}${markRemove}${name}: ${valueOld}${newline}`;
-};
-
-const actionModifiedDeepObj = (obj, level, func) => {
-  const { name, valueDeep } = obj;
-  return `${getIndent(level)}${indent}${name}: ${func(valueDeep, level + 1)}${newline}`;
-};
-
-const actions = {
-  modified: actionModified,
-  unmodified: actionUnmodified,
-  added: actionAdded,
-  removed: actionRemoved,
-  object: actionModifiedDeepObj,
-};
 
 const buildDiffString = (ast, level = 0) => {
-  const result = ast.map((obj) => {
-    const { type } = obj;
-    const action = actions[type];
-    return action(obj, level + 1, buildDiffString);
-  });
-
-  return `{${newline}${result.join('')}${getIndent(level)}}`;
+  const arrDiff = ast.map(obj => makeAction(obj, level + 1));
+  return `{${newline}${arrDiff.join('')}${getIndent(level)}}`;
 };
 
 
