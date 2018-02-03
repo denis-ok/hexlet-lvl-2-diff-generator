d5d34da13fd4dc6c0cb404aba506b1d162f961fb
denis-ok <strelkov.d.d@mail.ru>
2018-02-03 13:23:32 +0300

    refactor (plain.js)

--- src/renderers/plain.js
+++ src/renderers/plain.js
@@ -1,4 +1,4 @@
-/* eslint-disable object-curly-newline */
+/* eslint-disable object-curly-newline, no-use-before-define */
 import _ from 'lodash';
 
 const property = 'Property';
@@ -11,38 +11,36 @@ const compexValue = 'complex value';
 const value = 'value:';
 const newline = '\n';
 
-const buildDiffString = (ast, level = '') => {
-  const result = ast.map((obj) => {
-    const { name, type, valueNew, valueOld, valueDeep } = obj;
-    const newLevel = level === '' ? name : `${level}.${name}`;
+const makeAction = (obj, level) => {
+  const { name, type, valueNew, valueOld, valueDeep } = obj;
+  const newLevel = level === '' ? name : `${level}.${name}`;
 
-    const actionRemoved = () => `${property} '${newLevel}' ${wasRemoved}${newline}`;
-    const actionUnmodified = () => '';
-    const actionModified = () => `${property} '${newLevel}' ${wasUpdated} ${from} '${valueOld}' ${to} '${valueNew}'${newline}`;
-    const actionModifiedDeepObj = () => `${buildDiffString(valueDeep, `${newLevel}`)}`;
+  const actions = {
+    object: () => `${buildDiffString(valueDeep, `${newLevel}`)}`,
 
-    const actionAdded = () => {
+    unmodified: () => '',
+
+    removed: () => `${property} '${newLevel}' ${wasRemoved}${newline}`,
+
+    modified: () => `${property} '${newLevel}' ${wasUpdated} ${from} '${valueOld}' ${to} '${valueNew}'${newline}`,
+
+    added: () => {
       if (_.isObject(valueNew)) {
         return `${property} '${newLevel}' ${wasAddedWith} ${compexValue}${newline}`;
       }
-
       return `${property} '${newLevel}' ${wasAddedWith} ${value} '${valueNew}'${newline}`;
-    };
+    },
+  };
 
-    const actions = {
-      modified: actionModified,
-      unmodified: actionUnmodified,
-      added: actionAdded,
-      removed: actionRemoved,
-      object: actionModifiedDeepObj,
-    };
+  const getAction = actions[type];
+  return getAction();
+};
 
-    const makeAction = actions[type];
 
-    return makeAction();
-  });
-
+const buildDiffString = (ast, level = '') => {
+  const result = ast.map(obj => makeAction(obj, level));
   return result.join('');
 };
+
 
 export default buildDiffString;
